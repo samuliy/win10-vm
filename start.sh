b7c50bdeda9b106edba63d027b62a9ab0c028da3
Samuli Yliniemi <samuli.yliniemi@gmail.com>
2020-10-18 13:53:29 +0300

    Use single conf script, move vfio modprobe under PCI_PASSTHROUGH

--- start.sh
+++ start.sh
@@ -32,9 +32,6 @@ for ARG; do
 	esac
 done
 
-modprobe vfio
-modprobe vfio_pci
-
 if [[ -n "$HUGEPAGES" ]]; then
 	log "Setting up hugepages."
 
@@ -55,13 +52,17 @@ fi
 
 # PCI
 if [[ -n "$PCI_PASSTHROUGH" ]]; then
+	modprobe vfio || exit 2
+	modprobe vfio-pci || exit 2
+
 	source $SCRIPT_DIR/prompt/pci.sh
 fi
 
+CONF=$( $SCRIPT_DIR/conf/run.sh )
 if [[ -n "$INSTALL" ]]; then
-	CONF=$( $SCRIPT_DIR/conf/install.sh )
+	CONF="$CONF -boot order=dc"
 else
-	CONF=$( $SCRIPT_DIR/conf/run.sh )
+	CONF="$CONF -boot order=c"
 fi
 
 CMD="qemu-system-x86_64 $CONF"
