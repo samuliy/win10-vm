601af8e1b0361ee9956011313484c009a73c41bd
Samuli Yliniemi <samuli.yliniemi@gmail.com>
2020-10-18 20:18:35 +0300

    Add root PCIe bus device for GPU passthrough

--- components/pci.sh
+++ components/pci.sh
@@ -29,21 +29,59 @@ else
 		exit 2
 	fi
 
-	detach_console # For GPU passthrough
+	VGA_DEVICE=""
+	for DEVICE in $PCI_DEVICES; do
+		if lspci | grep $DEVICE | grep -q VGA ; then
+			# GPU passthrough
+			VGA_DEVICE=$DEVICE
+			detach_console
+		fi
+	done
+
+	ADDT_VGA_DEVICES=()
+	if [[ -n "$VGA_DEVICE" ]]; then
+		# Check if there are other functions in the same device.
+		for DEVICE in $PCI_DEVICES; do
+			if [[ $DEVICE = "$(echo $VGA_DEVICE | cut -d '.' -f 1)"* ]] && [[ $DEVICE != $VGA_DEVICE ]]; then
+				ADDT_VGA_DEVICES+=($DEVICE)
+			fi
+		done
+
+		# Add PCIe root bus device for GPU passthrough
+		echo "-device ioh3420,id=root_port1,chassis=0,slot=0,bus=pcie.0"
+		# Disable VGA and graphical UI
+		echo "-vga none -nographic"
+	fi
 
 	for DEVICE in $PCI_DEVICES; do
 		DEVICE_CONFIG="-device vfio-pci,host=$DEVICE"
 
 		bind_device $DEVICE vfio-pci
 
-		if lspci | grep $DEVICE | grep -q VGA ; then
-			DEVICE_CONFIG="$DEVICE_CONFIG,multifunction=on"
+		if [[ $DEVICE = $VGA_DEVICE ]]; then
+			# GPU passthrough
+			DEVICE_CONFIG="
+				$DEVICE_CONFIG,multifunction=on,bus=root_port1,addr=0x00
+			"
 		fi
+
+		ADDT_VGA_DEVICE_ADDR=1
+		for ADDT_VGA_DEVICE in "${ADDT_VGA_DEVICES[@]}"; do
+			if [[ $DEVICE = $ADDT_VGA_DEVICE ]]; then
+				# Add all the GPU functions to the same root bus device
+				DEVICE_CONFIG="
+					$DEVICE_CONFIG,bus=root_port1,addr=0x00.$ADDT_VGA_DEVICE_ADDR
+				"
+			fi
+			ADDT_VGA_DEVICE_ADDR=$(($ADDT_VGA_DEVICE_ADDR+1))
+		done
 
 		echo $DEVICE_CONFIG
 
-		unset DEVICE_CONFIG
+		unset DEVICE_CONFIG ADDT_VGA_DEVICE ADDT_VGA_DEVICE_ADDR
 	done
+
+	unset VGA_DEVICE ADDT_VGA_DEVICES
 
 	for DRIVER in $PCI_DRIVERS; do
 		unbind_driver $DRIVER
