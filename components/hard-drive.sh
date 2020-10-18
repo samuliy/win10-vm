f31042ce281562072700694d3dbbf28870b5e9ab
Samuli Yliniemi <samuli.yliniemi@gmail.com>
2020-10-18 18:31:16 +0300

    Increase hard drive size and add bootindex

--- components/hard-drive.sh
+++ components/hard-drive.sh
@@ -3,11 +3,18 @@
 HARD_DRIVE_IMG_FILE=/home/samuliy/sdc1/win10.img
 
 if [[ ! -f $HARD_DRIVE_IMG_FILE ]]; then
-	qemu-img create -f raw -o preallocation=full $HARD_DRIVE_IMG_FILE 40G > /dev/null
+	qemu-img create -f raw -o preallocation=full $HARD_DRIVE_IMG_FILE 80G > /dev/null
 fi
 
+HARD_DRIVE_BOOT_INDEX=1
+if [[ -n "$INSTALL" ]]; then
+	HARD_DRIVE_BOOT_INDEX=2
+fi
+
+HARD_DRIVE_ID=disk0
 HARD_DRIVE="
-	-drive id=disk0,if=virtio,cache=none,format=raw,file=$HARD_DRIVE_IMG_FILE
+	-drive id=$HARD_DRIVE_ID,if=none,cache=none,format=raw,file=$HARD_DRIVE_IMG_FILE
+	-device virtio-blk-pci,drive=$HARD_DRIVE_ID,bootindex=$HARD_DRIVE_BOOT_INDEX
 "
 
 echo $HARD_DRIVE
