02ef686badacd8ee5967d984cf8e34d4a4308431
Samuli Yliniemi <samuli.yliniemi@gmail.com>
2020-10-18 12:20:45 +0300

    Remove faulty log row from PCI prompt

--- prompt/pci.sh
+++ prompt/pci.sh
@@ -7,12 +7,12 @@ while read -r DEVICE <<< $(prompt_pci_device_list) ; do
 	if [[ -z "$DEVICE" ]]; then
 		break
 	fi
+
 	DRIVER=$(get_device_driver $DEVICE)
 	if [[ -z "$DRIVER" ]]; then
 		echo >&2 "Failed to get driver for device: $DEVICE!"
 		exit 2
 	fi
-	log "Device $DEVICE driver: $GPU_DRIVER."
 
 	PCI_DEVICES+=($DEVICE)
 	PCI_DRIVERS+=($DRIVER)
