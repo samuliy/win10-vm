b89d03f4906640355b8ad434ddd1579d6b17ee18
Samuli Yliniemi <samuli.yliniemi@gmail.com>
2020-10-18 18:31:49 +0300

    Add bootindex for install cd drive

--- conf/run.sh
+++ conf/run.sh
@@ -2,28 +2,28 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-BOOT="
--boot order=c
-"
-
 CD_INDEX=1
 CD_DRIVES=""
 
+INSTALL_DISK_BOOT_INDEX=2
 if [[ -n "$INSTALL" ]]; then
-	for ISO in $SCRIPT_DIR/../cd-drive-install/*.iso ; do
-		if [[ -z "$CD_BOOT_DEVICE_CREATED" ]]; then
-			CD_DRIVES="
-				$CD_DRIVES
-				-device ide-cd,bus=ide.0,unit=0,drive=drive-ide0,id=ide0-0-0,bootindex=1
-				-drive file=$ISO,index=$CD_INDEX,id=drive-ide0,if=none,format=raw,readonly=on
-			"
-			CD_INDEX=$(($CD_INDEX+1))
-		else
-			echo >&2 "Found multiple install CDs!"
-			exit 2
-		fi
-	done
+	INSTALL_DISK_BOOT_INDEX=1
 fi
+
+for ISO in $SCRIPT_DIR/../cd-drive-install/*.iso ; do
+	if [[ -z "$CD_BOOT_DEVICE_CREATED" ]]; then
+		CD_DRIVES="
+			$CD_DRIVES
+			-device ide-cd,bus=ide.0,unit=0,drive=drive-ide0,id=ide0-0-0,bootindex=$INSTALL_DISK_BOOT_INDEX
+			-drive file=$ISO,index=$CD_INDEX,id=drive-ide0,if=none,format=raw,readonly=on
+		"
+		CD_INDEX=$(($CD_INDEX+1))
+		CD_BOOT_DEVICE_CREATED=1
+	else
+		echo >&2 "Found multiple install CDs!"
+		exit 2
+	fi
+done
 
 for ISO in $SCRIPT_DIR/../cd-drive/*.iso ; do
 	CD_DRIVES="
