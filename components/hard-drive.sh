d315455b00f9baf06629d2c7fb21d88955a9809b
Samuli Yliniemi <samuli.yliniemi@gmail.com>
2020-10-27 09:07:24 +0200

    Change hard drive type

--- components/hard-drive.sh
+++ components/hard-drive.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-HARD_DRIVE_IMG_FILE=/home/samuliy/sdc1/win10.img
-
-if [[ ! -f $HARD_DRIVE_IMG_FILE ]]; then
-	qemu-img create -f raw -o preallocation=full $HARD_DRIVE_IMG_FILE 80G > /dev/null
-fi
-
+HARD_DRIVE_UUID=/dev/sdd
 HARD_DRIVE_BOOT_INDEX=1
 if [[ -n "$INSTALL" ]]; then
 	HARD_DRIVE_BOOT_INDEX=2
 fi
 
-HARD_DRIVE_ID=disk0
 HARD_DRIVE="
-	-drive id=$HARD_DRIVE_ID,if=none,cache=none,format=raw,file=$HARD_DRIVE_IMG_FILE
-	-device virtio-blk-pci,drive=$HARD_DRIVE_ID,bootindex=$HARD_DRIVE_BOOT_INDEX
+	-drive file=$HARD_DRIVE_DEV,format=raw,if=none,id=drive-virtio-disk0,cache=none,aio=native
+	-device virtio-blk-pci,scsi=off,drive=drive-virtio-disk0,id=virtio-disk0,bootindex=$HARD_DRIVE_BOOT_INDEX
 "
 
 echo $HARD_DRIVE
 
-unset HARD_DRIVE HARD_DRIVE_IMG_FILE
+unset HARD_DRIVE HARD_DRIVE_DEV
