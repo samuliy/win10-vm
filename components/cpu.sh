9c3a1ba9c984328390562fe239173856d25506c2
Samuli Yliniemi <samuli.yliniemi@gmail.com>
2020-10-28 20:02:54 +0200

    Use cpu count from the output of lscpu

--- components/cpu.sh
+++ components/cpu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VCPU_NUM=8
+VCPU_COUNT=$(lscpu | grep -P "^CPU\(s\):\s+" | awk '{ print $2 }')
 
 CPU="
-	-smp $VCPU_NUM,sockets=1,cores=$VCPU_NUM,threads=1
+	-smp $VCPU_COUNT,sockets=1,cores=$VCPU_COUNT,threads=1
 	-cpu host,kvm=off,hv_vendor_id=1234567890ab,hv_vapic,hv_time,hv_relaxed,hv_spinlocks=0x1fff,+vmx
 "
 
