ef966e260caf294b5364c77b3ebeb4239fd6491e
Samuli Yliniemi <samuli.yliniemi@gmail.com>
2020-10-28 20:03:07 +0200

    Modify start script
    Use memory amount variable for hugepages.
    Set thread priorities after qemu has started.

--- start.sh
+++ start.sh
@@ -27,6 +27,7 @@ for ARG; do
 		--hugepages)
 			log "Using Hugepages."
 			export HUGEPAGES=1
+			export MEM_AMOUNT=14336
 		;;
 	esac
 done
@@ -34,12 +35,16 @@ done
 if [[ -n "$HUGEPAGES" ]]; then
 	log "Setting up hugepages."
 
+	# Flush buffers and drop caches
 	sync
 	echo 1 > /proc/sys/vm/drop_caches
-	# defrag ram
+
+	# Defrag ram
 	echo 1 > /proc/sys/vm/compact_memory
-	# assign hugepages
-	sysctl -w vm.nr_hugepages=6144
+
+	# Assign hugepages
+	echo "never" > /sys/kernel/mm/transparent_hugepage/enabled
+	sysctl -w vm.nr_hugepages=$((MEM_AMOUNT/2))
 
 	log "Hugepages set up."
 fi
@@ -58,6 +63,26 @@ if [[ -n "$PCI_PASSTHROUGH" ]]; then
 
 	source $SCRIPT_DIR/prompt/pci.sh
 fi
+
+(
+	SECONDS=0
+	while ! ps -e | grep -q qemu-system-x86_64 ; do
+		sleep 1
+		if [[ $SECONDS -gt 60 ]]; then
+			echo >&2 "Timed out while waiting for qemu to be up!"
+			exit 2
+		fi
+	done
+
+	QEMU_NICE=-10
+	QEMU_CHRT_PRI=1
+
+	PROC_PID=$(ps -o 'pid=' -o 'comm=' | grep qemu-system-x86_64 | awk '{ print $1 }')
+	for THREAD_PID in $(ps -T -H -o 'tid=' $PROC_PID | awk '{ print $1 }'); do
+		renice -n $QEMU_NICE -p $THREAD_PID
+		chrt --rr --pid $QEMU_CHRT_PRI $THREAD_PID
+	done
+) &
 
 CONF=$( $SCRIPT_DIR/conf/run.sh )
 CMD="qemu-system-x86_64 $CONF"
