c58cf7b66de2925c5824e6c8c32d7fdd10058c8e
Samuli Yliniemi <samuli.yliniemi@gmail.com>
2020-11-28 08:42:59 +0200

    Change cpu flags

--- components/cpu.sh
+++ components/cpu.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 VCPU_COUNT=$(lscpu | grep -P "^CPU\(s\):\s+" | awk '{ print $2 }')
-VCPU_COUNT=$(($VCPU_COUNT-1))
 
 CPU="
 	-smp $VCPU_COUNT,sockets=1,cores=$VCPU_COUNT,threads=1
-	-cpu host,kvm=off,hv_vendor_id=1234567890ab,hv_vapic,hv_time,hv_relaxed,hv_spinlocks=0x1fff,+vmx
+	-cpu host,topoext=on,host-cache-info=on,kvm=off,hv_vendor_id=1234567890ab,hv_vapic,hv_time,hv_relaxed,hv_spinlocks=0x1fff,l3-cache=on,-hypervisor,migratable=no,+invtsc,+vmx
 "
 
 echo $CPU
